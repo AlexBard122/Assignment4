7e642eaea78e7fbbe616738e47b290e438b78f45
AlexBard122 <alexander.bard@maine.edu>
2024-02-15 14:08:24 -0600

    update program4
    updated program4 main method with comments

--- src/accidentpack/program4.java
+++ src/accidentpack/program4.java
@@ -10,11 +10,24 @@ package accidentpack;
 public class program4 {
 
 	/**
+	 * @author abard
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/**
+		 * need to take in csv file, will use modofied version of methods from assignment2 to do so
+		 *  arraylist will be sorted based on start time (ascending)
+		 *  arraylist will likely be called "report" 
+		 *  I will probably call your method with arguments String state, String county
+		 *  
+		 *  Let me know if you think I should change something
+		 *  Also, I suggest that we should push and merge daily (if we made modifications that day)
+		 *  	so that we can stay updated
+		 *  
+		 *  Also, it might be a good idea to exchange phone numbers so we can text one another for something urgent
+		 */
 	}
 
 }
