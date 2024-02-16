78c7bd9367540a7f285da57532f6d674306e5c34
Devin-C285 <devin.chamberlain@maine.edu>
2024-02-16 10:23:55 -0500

    created helper class

--- src/accidentpack/program4.java
+++ src/accidentpack/program4.java
@@ -2,6 +2,12 @@
  * 
  */
 package accidentpack;
+<<<<<<< Updated upstream
+=======
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+>>>>>>> Stashed changes
 
 /**
  * @author abard
@@ -10,11 +16,68 @@ package accidentpack;
 public class program4 {
 
 	/**
+<<<<<<< Updated upstream
+=======
+	 * @author abard
+	 * calculates elapsed time and converts it to seconds
+	 * @param time1
+	 * @param time2
+	 * @return String
+	 */
+	public static String convertTime(long time1, long time2) {
+		long elapsedTime = time2 - time1;
+		double elapsedTimeSeconds;
+		elapsedTimeSeconds = elapsedTime / 1000000000;
+		return String.valueOf(elapsedTimeSeconds);
+	}
+	/**
+	 * @author abard
+>>>>>>> Stashed changes
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+<<<<<<< Updated upstream
+=======
+		/**
+		 * I have provided the methods main, convertTime (to count time between tasks)
+		 * 	ReadCSVFile (to read the file), readfile (converts contents of file into reports and
+		 * 	adds them to an ArrayList) and sortfile (sorts the arraylist by start time as reports are added)
+		 * 
+		 * I am using my report constructor class because I know it works with my sorting methods, but I can change
+		 * 	aspects of it if needed
+		 * 
+		 * Program needs arguments in order to run. make sure to set run configurations arguments to ${string_prompt}
+		 * 	and use the input: accidents_small_sample.csv county state
+		 */
+		
+		// user-generated arguments
+        String filePath = args[0];
+        String county = args[1];
+        String state = args[2];
+        
+        // reading the csv file
+        ArrayList<report> report = reportHelper.ReadCSVFile(filePath);
+        
+        //loop used to print start times to show ArrayList is sorted properly. feel free to remove
+        for(report reportlist: report) {
+        	System.out.println(reportlist.getStartTime() + "\t" + reportlist.getCounty());
+        }
+        
+        ArrayList<report> filtered = reportHelper.getReportByCountyAndState(report, county, state);
+        for(report reports: filtered) {
+            System.out.println(reports.getCounty() + " " + reports.getState());
+        }
+        
+        //calculateCounters(report, county, state);
+        ArrayDeque<Integer> countersNeeded = new ArrayDeque<>();
+        countersNeeded = reportHelper.calculateCounters(report, county, state);
+        
+        for(Integer counters: countersNeeded) {
+            System.out.println(counters);
+        }
+>>>>>>> Stashed changes
 	}
 
 }
---
+++ src/accidentpack/reportHelper.java
@@ -0,0 +1,152 @@
+/**
+ * Class containing helper methods for operating upon report objects.
+ * @author Alex Bard & Devin Chamberlain
+ * @version 16 February, 2024
+ */
+package accidentpack;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class reportHelper {
+    
+    /**
+     * @author abard
+     * reads a CSV file and populates an ArrayList of report objects
+     * @param filePath
+     * @return Array List of report objects
+     * @throws IOException
+     */
+    public static ArrayList<report> ReadCSVFile(String filePath) throws IOException
+    {
+        ArrayList<report> report = new ArrayList<report>();
+        
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line = null;
+        
+        reader.readLine();
+        while ((line = reader.readLine()) != null) {
+         
+        readfile(report, line);
+        }
+        reader.close();
+        return report;
+    }
+    /**
+     * @author abard
+     * sorts the contents of a csv file into and ArrayList<report>
+     * @param report
+     * @param line
+     */
+    private static void readfile(ArrayList<report> report, String line) {
+        String[] items = line.split(",");
+        String ID = items[0];
+        int Severity = Integer.parseInt(items[1]);
+        String StartTime = (items[2]);
+        String EndTime = (items[3]);
+        String Street = items[4];
+        String City = items[5];
+        String County = items[6];
+        String State = items[7];
+        String Temp = (items[8]);
+        String Humidity = (items[9]);
+        double Visibility = Double.parseDouble(items[10]);
+        String Weather = items[11];
+        boolean Crossing = Boolean.parseBoolean(items[12]);
+        String Daynight = items[13];
+        report r = new report(ID, Severity, StartTime, EndTime, Street, City, County, State, Temp,
+                Humidity, Visibility, Weather, Crossing, Daynight);
+        
+        sortfile(report, r);
+        //report.add(r);
+    }
+    
+    /**
+     * @author abard
+     * method for helping sort the ArrayList by start time in ascending order
+     * @param ArrayList<report>
+     * @param report
+     */
+    private static void sortfile(ArrayList<report> reports, report r) {
+        if(reports.size() == 0) {
+            reports.add(r);
+            return;
+        }
+        int index = 0;
+        for(report reportlist: reports) {
+            //if a start time in the list is greater than the provided start time, then add r before it
+            if(reportlist.getStartTime().compareTo(r.getStartTime()) > 0) {
+                reports.add(index, r);
+                return;
+            }
+            index++;
+        }
+        reports.add(r);
+    }
+    
+    /**
+     * @author Devin
+     * Provides an ArrayList of Report objects that took place in specified County 
+     * and State from given ArrayList
+     * @param reports, an Arraylist of Report objects
+     * @param county, The County to match
+     * @param state, The State to match
+     * @return result, The ArrayList of Reports that match <code>county</code> and <code>state</code> 
+     */
+    public static ArrayList<report> getReportByCountyAndState(ArrayList<report> reports, String county, String state) {
+        ArrayList<report> result = new ArrayList<>();
+        for (report report: reports) {
+            if (report.getCounty().equals(county) && report.getState().equals(state)) {
+                result.add(report);
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * @author Devin
+     * Calculates the number of counters needed to process reports in given <code>county</code> 
+     * and <code>state</code> for each individual day and puts them in a queue.
+     * @param reports The list of reports to be filtered and processed
+     * @param county The County to filter reports by
+     * @param state The State to filter reports by
+     * @return a queue in the form of an ArrayDeque of the number of counters needed for each day
+     */
+    public static ArrayDeque<Integer> calculateCounters(ArrayList<report> reports, String county, String state) {
+        //call helper method to get arraylist of reports filtered 
+        //by County and State, and sorted in ascending order
+        ArrayList<report> filteredList = getReportByCountyAndState(reports, county, state);
+        //create an ArrayList to hold each day's counters
+        ArrayDeque<Integer> numCounters = new ArrayDeque<>();
+        String startDate = filteredList.get(0).getStartTime().substring(0, 10);
+        String endDate = filteredList.get(filteredList.size()-1).getStartTime().substring(0, 10);
+        String currentDate = startDate;
+        Iterator<report> itr = filteredList.iterator();
+        ArrayDeque<report> dayCounter = new ArrayDeque<>();
+        
+        while (!filteredList.isEmpty()) {
+            while (itr.hasNext()) {
+                report nextReport = itr.next();
+                if (nextReport.getStartTime().substring(0, 10).equals(currentDate));
+                dayCounter.addLast(nextReport);
+                itr.remove();
+            }
+            if (currentDate != endDate) {
+                currentDate = filteredList.get(0).getStartTime().substring(0, 10);
+            }
+            int minutes = 0;
+            while (!dayCounter.isEmpty()) {
+                report nextReport = dayCounter.pollFirst();
+                minutes += (nextReport.getSeverity()*60);            
+            }
+            //learned from: https://www.programiz.com/java-programming/library/math/ceil
+            int counters = (int) Math.ceil(minutes / 1440);
+            numCounters.addLast(counters);
+        }
+        return numCounters;
+    }
+}
