f84b4aca1f7686f6e468bf89f862c589aa70957c
AlexBard122 <alexander.bard@maine.edu>
2024-02-19 10:56:00 -0600

    Alex finished 2
    just removed the collections import, it was unneeded

--- src/accidentpack/reportHelper.java
+++ src/accidentpack/reportHelper.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class reportHelper {
     
